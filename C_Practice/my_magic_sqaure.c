c1f96fea6449170d3894706775948f3f68e5beab
HackCodeWorld <147301478+HackCodeWorld@users.noreply.github.com>
2023-10-09 01:57:47 -0500

    Update my_magic_sqaure.c

--- C_Practice/my_magic_sqaure.c
+++ C_Practice/my_magic_sqaure.c
@@ -1,6 +1,6 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 // Structure that represents a magic square
 typedef struct {
@@ -8,113 +8,173 @@ typedef struct {
     int **magic_square; // pointer to heap allocated magic square
 } MagicSquare;
 
-// 获取魔方的大小，确保它是大于或等于3的奇数。
+
+/**
+ * Prompts the user for the magic square's size, reads it, checks if it's an odd number >= 3.
+ * If not valid, it displays the required error message.
+ * Pre-conditions: None
+ * retval: Size inputted by the user (may not be valid).
+ */
 int getSize() {
-    int n;
-    // 提示用户输入魔方的大小
-    printf("输入魔方的大小（奇数整数 >=3）\n");
+    int n; // size of the square
+
+    // initial prompt
+    printf("Enter magic square's size (odd integer >=3)\n");
     scanf("%d", &n);
-    // 检查输入是否为奇数并且大于等于3
-    while (n < 3 || n % 2 == 0) {
-        if (n < 3) {
-            printf("魔方大小必须大于等于 3。\n");
-        } else {
-            printf("魔方大小必须是奇数。\n");
-        }
-        printf("输入魔方的大小（奇数整数 >=3）\n");
-        scanf("%d", &n);
+
+    // check even
+    if (n % 2 == 0) {
+        printf("Magic square size must be odd.\n");
     }
+    // check size >= 3
+    else if (n < 3) {
+        printf("Magic square size must be >= 3.\n");
+    }
+
     return n;
 }
 
-// 使用“连体法”算法生成大小为n的魔方
+/**
+ * Adjusts the indices of a magic square's row and column to ensure they fall within
+ * the boundaries of the magic square.
+ *
+ * Pre-conditions:
+ * *i: Initial row index.
+ * *j: Initial column index.
+ * n: Size of the magic square.
+ * Post-conditions: *i and *j are modified to remain within the boundaries [0, n-1].
+ */
+void boundary_check(int *i, int *j, int n) {
+    *i = (*i + n) % n;  // adjust row index within boundary.
+    *j = (*j + n) % n;  // adjust column index within boundary.
+}
+
+/**
+ * Creates and returns a magic square of size 'n'.
+ *
+ * Pre-conditions:
+ * n: Size of the desired magic square. Should be an odd integer >= 3.
+ * retval: Pointer to the completed MagicSquare structure.
+ */
 MagicSquare *generateMagicSquare(int n) {
-    // 动态分配魔方结构的内存
+    // dynamically allocate the memory space for MagicSquare structure
     MagicSquare *ms = (MagicSquare *) malloc(sizeof(MagicSquare));
+    if (ms == NULL) {
+        printf("Error: Memory allocation for MagicSquare structure failed!\n");
+        exit(1);
+    }
     ms->size = n;
-    // 动态分配矩阵的行
+    // dynamically allocate the memory space for MagicSquare structure's matrix rows
     ms->magic_square = (int **) malloc(n * sizeof(int *));
+    if (ms->magic_square == NULL) {
+        printf("Error: Memory allocation for pointer to heap allocated magic square failed!\n");
+        exit(1);
+    }
     for (int i = 0; i < n; i++) {
-        // 动态分配每行的列
+        // dynamically allocate the memory space for MagicSquare structure's
+        // matrix rows' columns
         *(ms->magic_square + i) = (int *) malloc(n * sizeof(int));
+        if (*(ms->magic_square + i) == NULL) {
+            printf("Error: Memory allocation for MagicSquare structure's matrix row's columns failed!\n");
+            exit(1);
+        }
+        // initialize all elements to default 0
+        for (int j = 0; j < n; j++) {
+            *(*(ms->magic_square + i) + j) = 0;
+        }
     }
 
-    int num = 1;  // 从1开始填充
-    int i = 0, j = n / 2;  // 初始化为顶部中间位置
-    // 循环直到魔方中填充了所有的数字（即从1到n^2）
+    // started from 1
+    int num = 1;
+    // column in the middle for the base step
+    int i = 0, j = n / 2;
+
+    // iterate through number 1 to n^2
     while (num <= n * n) {
-        // 在当前位置放置数字
-        *(*(ms->magic_square + i) + j) = num++;
+        // setup constraints and boundary check for memory
+        boundary_check(&i, &j, n);
 
-        // 默认情况下，我们将沿着右上方向移动
-        i--;
-        j++;
+        // conflict with existent number
+        if (*(*(ms->magic_square + i) + j) != 0) {
+            // recover column, num shifts down by 1
+            i = (i + 2) % n;  // Adjust row
+            j = (j - 1 + n) % n;  // Adjust column
+            // + n will not affect the val but keep it positive
+        } else {
+            // assign value to the magic_square
+            *(*(ms->magic_square + i) + j) = num++;
 
-        // 边界和冲突处理：
-        // 如果我们达到了正方形的左上角
-        if (i < 0 && j >= n) {
-            // 移动到正下方
-            i += 2;
-            j--;
-        }
-            // 如果我们在正方形的上边界
-        else if (i < 0) {
-            // 我们“绕回”到最底部
-            i = n - 1;
-        }
-            // 如果我们在正方形的右边界
-        else if (j >= n) {
-            // 我们“绕回”到最左边
-            j = 0;
-        }
-            // 如果当前位置已经有数字了（冲突）
-        else if (*(*(ms->magic_square + i) + j) != 0) {
-            // 我们移动到当前位置的正下方
-            i += 2;
-            j--;
+            // move towards up and right
+            i--;
+            j++;
         }
     }
 
     return ms;
 }
 
-// 将魔方写入指定的文件
+/**
+ * Writes the values of the given magic square to a file.
+ *
+ * Pre-conditions:
+ * magic_square: Pointer to the magic square structure to write to the file.
+ * filename: Name of the output file.
+ * Post-conditions: A file named 'filename' will be created or overwritten with the magic square values.
+ */
 void fileOutputMagicSquare(MagicSquare *magic_square, char *filename) {
-    // 打开输出文件
+    // open output file
     FILE *fp = fopen(filename, "w");
     if (!fp) {
-        printf("Error opening file.\n");
+        printf("Error opening file to write!\n");
         exit(1);
     }
-    // 写入魔方的大小
+    // write the file size at first
     fprintf(fp, "%d\n", magic_square->size);
     for (int i = 0; i < magic_square->size; i++) {
         for (int j = 0; j < magic_square->size; j++) {
-            // 写入魔方中的每个数字
+            // write each num in square
             fprintf(fp, "%d", *(*(magic_square->magic_square + i) + j));
-            if (j != magic_square->size - 1) fprintf(fp, ",");
+            if (j != magic_square->size - 1) {
+                fprintf(fp, ",");
+            }
         }
         fprintf(fp, "\n");
     }
-    fclose(fp);  // 关闭文件
+
+    // close any opened files when it's done using them
+    if (fclose(fp) == EOF) {
+        perror("Error while closing the file");
+        exit(1);
+    }
 }
 
-// 主函数，组织程序流程
+/**
+ * Main execution point. Generates a magic square of user-specified size and outputs the square
+ * to the output filename given as a command line argument.
+ *
+ * Usage:
+ * ./program_name [output_file_name]
+ * Pre-conditions:
+ * argc: Total number of command-line arguments, should be 2.
+ * argv[0]: Program's name.
+ * argv[1]: Name of the output file.
+ * retval: 0 for successful execution, 1 if there's an error.
+ */
 int main(int argc, char **argv) {
-    // 检查命令行参数是否正确
+    // check argc counts correct
     if (argc != 2) {
-        printf("使用方法： ./my_magic_square <输出文件名>。\n");
+        printf("Usage: ./my_magic_square <output_filename>\n");
         return 1;
     }
 
-    // 从用户那里获取魔方的大小
+    // get the size of the magic square from the user
     int n = getSize();
-    // 生成魔方
+    // generate the magic square
     MagicSquare *ms = generateMagicSquare(n);
-    // 将魔方输出到文件
+    // write to the output file
     fileOutputMagicSquare(ms, argv[1]);
 
-    // 释放所有动态分配的内存
+    // free all heap memory reversely
     for (int i = 0; i < n; i++) {
         free(*(ms->magic_square + i));
     }
@@ -123,4 +183,3 @@ int main(int argc, char **argv) {
 
     return 0;
 }
-
