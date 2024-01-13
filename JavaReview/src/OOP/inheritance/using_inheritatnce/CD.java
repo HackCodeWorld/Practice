5be98c847c6dc9d25951706f398fb78e6276725d
amg <ballerbrabus@outlook.com>
2024-01-12 22:29:05 -0600

    updated some pratice of inheritance

--- JavaReview/src/OOP/inheritance/using_inheritatnce/CD.java
+++ JavaReview/src/OOP/inheritance/using_inheritatnce/CD.java
@@ -2,8 +2,12 @@ package OOP.inheritance.using_inheritatnce;
 
 /**
  * CD is 子类 of Item
- * inherit everything from farther Item
+ * inherit everything from farther-父类 -> Item
  * AND can add more things about itself
+ * AND child子类可以change父类private fields if using father's function
+ * AND child子类的attributes包含farther的copies of attributes
+ * AND IF child子类's field with same name as 父类 then it is the one in child
+ * AND IF executing in 父类 it is the one in 父类
  */
 public class CD extends Item {
     private String artist;
@@ -14,6 +18,7 @@ public class CD extends Item {
         super(title, playingTime, comment);
         this.artist = artist;
     }
+
     public static void main(String[] args) {
 
     }
--- JavaReview/src/OOP/inheritance/using_inheritatnce/DVD.java
+++ JavaReview/src/OOP/inheritance/using_inheritatnce/DVD.java
@@ -1,9 +1,13 @@
 package OOP.inheritance.using_inheritatnce;
 
 /**
- * DVD is 子类 of Item
- * inherit everything from farther Item
+ * DVD is 子类-child of Item
+ * inherit everything from farther-父类 -> Item
  * AND can add more things about itself
+ * AND child子类可以change父类private fields if using father's function
+ * AND child子类的attributes包含farther的copies of attributes
+ * AND IF child子类's field with same name as 父类 then it is the one in child
+ * AND IF executing in 父类 it is the one in 父类
  */
 public class DVD extends Item{
     private String director;
